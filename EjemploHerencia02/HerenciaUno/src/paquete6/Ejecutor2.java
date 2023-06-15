c1fb74c5be7d4328718970213d23b56e87693ed7
nahomi2004 <soy.nahomi.mucho.gusto@gmail.com>
2023-06-15 12:03:41 -0500

    Si Aprendi 3.0

--- EjemploHerencia02/HerenciaUno/src/paquete6/Ejecutor2.java
+++ EjemploHerencia02/HerenciaUno/src/paquete6/Ejecutor2.java
@@ -19,25 +19,44 @@ public class Ejecutor2 {
         // Debe existir 1 sola impresión de todos los estudiantes presenciales
         // No usar arreglos
         
-        // Trabajo clases 16 junio 2022
+        // Trabajo clases 15 junio 2022
         Scanner entrada = new Scanner(System.in);
         boolean bandera = true;
         String cadena = "";
         
+        System.out.println("Ingrese estudiante/s");
+        
         while(bandera){
-            System.out.println("ingresa un saludo");
-            String saludo = entrada.nextLine();
-            cadena = String.format("%s%s\n", cadena, saludo);
-            System.out.println("Desea salir, pulse S");
+            System.out.println("Ingrese nombre:");
+            String nombre = entrada.nextLine();
+            System.out.println("Ingrese apellido:");
+            String apellido = entrada.nextLine();
+            System.out.println("Ingrese id:");
+            String id = entrada.nextLine();
+            System.out.println("Ingrese edad:");
+            int edad = entrada.nextInt();
+            System.out.println("Ingrese numero de creditos:");
+            int credit = entrada.nextInt();
+            System.out.println("Ingrese costo de los credito:");
+            double costo = entrada.nextInt();
+            
+            EstudiantePresencial estPresencial = new EstudiantePresencial(nombre,
+                apellido, id, edad, costo, credit);
+            
+            estPresencial.calcularMatriculaPresencial();
+            
+            cadena = String.format("\n%s\n%s\n",cadena, estPresencial);
+            
+            entrada.nextLine();
+            System.out.println("Desea salir, pulse Si");
             String opcion = entrada.nextLine();
-            if(opcion.equals("S")){
+            if(opcion.equals("Si")){
                 bandera = false;
             }
         }
         
+        System.out.println("ESTUDIANTE/S:");
         System.out.printf("%s\n", cadena);
-        
-        
     }
 }
 
